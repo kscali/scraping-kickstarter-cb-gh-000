10a7be6d23423d0634c6baec2be56d45cce0a911
Koy Saeteurn <kosatun@aol.com>
2018-08-03 05:41:14 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,8 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   
-  project = kickstarter.css(".project grid 4")
+  project = kickstarter.css(".project grid_4")
+  
 
 
 
